ec116586b508ea3d7b509360cba63c1d8b254747
RealTimeCom <expertphp@gmail.com>
2017-01-27 03:48:28 +0200

    0.3.1

--- index.js
+++ index.js
@@ -3,76 +3,65 @@
 
 const Transform = require('stream').Transform;
 
+const cache = { // cache common values
+    n: Buffer.from('\r\n'), // header-body separator
+    z: Buffer.allocUnsafeSlow(0) // create an un-pooled empty buffer
+};
+const init = { // init object values
+    h: true, // data is header?
+    p: {}, // data header request cache
+    w: true // connection is open?
+};
+
 class server extends Transform {
     constructor(f) {
         super();
+        Object.assign(this, init);
         this.f = typeof f === 'function' ? f.bind(this) : (response, head, body) => response(head, body); // cache server function
         this.e = 'serverError'; // custom error event name
-        this.n = Buffer.from('\r\n');
-        this.h = true; // data is header?
-        this.z = Buffer.allocUnsafeSlow(0); // create an un-pooled empty buffer
-        this.c = this.z; // init empty cache buffer
-        this.p = {}; // data header request cache
-        this.w = true; // connection is open?
+        this.c = cache.z; // init empty cache buffer
     }
 }
-server.prototype._transform = function(chunk, enc, cb) {
-    parse.bind(this)(chunk, true); // parse chunk, true = is server
-    cb();
-};
-server.prototype._flush = function(cb) {
-    this.w = false; // connection closed
-    if (this.c.length > 0) { parse.bind(this)(this.z, true); } // parse data left, true = is server
-    cb();
-};
+server.prototype._transform = trans;
+server.prototype._flush = flush;
 
 class client extends Transform {
     constructor() {
         super();
+        Object.assign(this, init);
         this.f = undefined;
         this.e = 'clientError'; // custom error event name
-        this.n = Buffer.from('\r\n');
-        this.h = true; // data is header?
-        this.z = Buffer.allocUnsafeSlow(0); // create an un-pooled empty buffer
-        this.c = this.z; // init empty cache buffer
-        this.p = {}; // data header request cache
-        this.w = true; // connection is open?
+        this.c = cache.z; // init empty cache buffer
     }
 }
-client.prototype._transform = function(chunk, enc, cb) {
-    parse.bind(this)(chunk, false); // parse chunk, false = is client
-    cb();
-};
-client.prototype._flush = function(cb) {
-    this.w = false; // connection closed
-    if (this.c.length > 0) { parse.bind(this)(this.z, false); } // parse data left, false = is client
-    cb();
-};
+client.prototype._transform = trans;
+client.prototype._flush = flush;
+
 client.prototype.exec = function(f, head, body) {
     this.f = typeof f === 'function' ? f.bind(this) : undefined; // cache client function
     send.bind(this)(head, body);
     return this;
 };
 
-function parse(chunk, server) { // type server ?
+function parse(chunk) {
     this.c = Buffer.concat([this.c, chunk]);
     if (this.h) { // chunk is header
         this.p = {}; // init/reset this.p
-        const i = this.c.indexOf(this.n); // search for separator
+        const i = this.c.indexOf(cache.n); // search for separator
         if (i !== -1) { // separator is found
             const h = this.c.slice(0, i).toString().trim();
             try { // try JSON.parse()
                 const p = JSON.parse(h);
                 if ('h' in p && 'l' in p && typeof p.l === 'number' && p.l >= 0) {
-                    const body = this.c.slice(i + this.n.length);
+                    const body = this.c.slice(i + cache.n.length);
                     if (body.length >= p.l) { // body complete
                         if (body.length > p.l) {
                             body = body.slice(0, p.l);
                             this.c = body.slice(p.l); // cache remaining bytes, for the next request
                         } else { // empty cache
-                            this.c = this.z;
+                            this.c = cache.z;
                         }
-                        if (server) {
+                        if (this.e === 'serverError') { // is server
                             if (this._readableState.pipes.pause) { this._readableState.pipes.pause(); } // pause socket until server response back
                             this.f(send.bind(this), p.h, body);
                         } else {
@@ -85,12 +74,12 @@ function parse(chunk, server) { // type server ?
                     }
                 } else {
                     this.emit(this.e, new Error('Invalid Header'));
-                    this.c = this.z;
+                    this.c = cache.z;
                     if (this.w) { this.push(null); }
                 }
             } catch (e) {
                 this.emit(this.e, e);
-                this.c = this.z;
+                this.c = cache.z;
                 if (this.w) { this.push(null); }
             }
         } // need more data
@@ -102,10 +91,10 @@ function parse(chunk, server) { // type server ?
                 this.c = this.c.slice(this.p.l); // cache remaining bytes, for next request
             } else {
                 body = this.c;
-                this.c = this.z; // empty cache
+                this.c = cache.z; // empty cache
             }
             this.h = true; // next chunk is header
-            if (server) { // is server
+            if (this.e === 'serverError') { // is server
                 if (this._readableState.pipes.pause) { this._readableState.pipes.pause(); } // pause socket until server response back
                 this.f(send.bind(this), this.p.h, body); // call server function
             } else { // is client
@@ -117,23 +106,29 @@ function parse(chunk, server) { // type server ?
 
 function send(head, body) {
     if (this.w) {
-        if (body === undefined) { body = this.z; }
-        if (!Buffer.isBuffer(body)) {
-            if (typeof body === 'string') {
-                body = Buffer.from(body);
-            } else { // try convert body to String
-                body = Buffer.from(body + '');
-            }
+        if (body === undefined) { body = cache.z; }
+        if (!Buffer.isBuffer(body)) { // convert body value into Buffer
+            body = typeof body === 'string' ? Buffer.from(body) : Buffer.from(body + '');
         }
         try { // try JSON.stringify()
-            this.push(Buffer.concat([Buffer.from(JSON.stringify({ h: head, l: body.length })), this.n, body]));
+            this.push(Buffer.concat([Buffer.from(JSON.stringify({ h: head, l: body.length })), cache.n, body]));
             if (this._readableState.pipes.resume) { this._readableState.pipes.resume(); } // resume socket, get more data
         } catch (e) {
             this.emit(this.e, e);
-            this.c = this.z;
+            this.c = cache.z;
             this.push(null);
         }
     }
+}
+
+function trans(chunk, enc, cb) {
+    parse.bind(this)(chunk); // parse chunk
+    cb();
+}
+function flush(cb) {
+    this.w = false; // connection closed
+    if (this.c.length > 0) { parse.bind(this)(cache.z); } // parse data left
+    cb();
 }
 
 module.exports = { server: server, client: client };
